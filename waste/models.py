4a5370d58055564541e3cb96a13231c3dbf701ad
Ak-shay <mittalakshay2000@gmail.com>
2019-03-30 11:19:05 +0530

    changes

--- waste/models.py
+++ waste/models.py
@@ -1,30 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Country(models.Model):
     country_name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.country_name
 
 class State(models.Model):
     state_name = models.CharField(max_length=30)
     country_name = models.ForeignKey(Country, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.state_name
 
 class City(models.Model):
     city_name = models.CharField(max_length=30)
     state_name = models.ForeignKey(State, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.city_name
 
 class People(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     name = models.CharField(max_length = 50)
     pic = models.ImageField()
     city = models.ForeignKey(City,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Worker(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     name = models.CharField(max_length = 50)
     pic = models.ImageField()
     city = models.ForeignKey(City,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Bin(models.Model):
     citizen = models.ForeignKey(People,on_delete=models.CASCADE)
     green_waste = models.IntegerField()
     blue_waste = models.IntegerField()
+    time = models.DateTimeField(null=True,blank=True)
+    def __str__(self):
+        return self.citizen
+
+    def send(self):
+        self.time = timezone.now()
+        self.save()
--- waste/views.py
+++ waste/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import People,Country,City,Bin,State,Worker
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def index(request):
+    pass
