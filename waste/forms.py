1624f22dd3808bee3279e0e8c64d36fb20e81a4a
Ak-shay <mittalakshay2000@gmail.com>
2019-03-30 16:30:06 +0530

    registration

--- waste/forms.py
+++ waste/forms.py
@@ -6,6 +6,12 @@ class CleanGarbageForm(forms.ModelForm):#filled by people
 		model=Bin
 		fields = ('green_waste','blue_waste')
 
-		
+class PeopleForm(forms.ModelForm):
+	class Meta:
+		model = People
+		fields = ('name','pic','city')
 
-
+class WorkerForm(forms.ModelForm):
+	class Meta:
+		model = Worker
+		fields = ('name','pic','city')
