46b46f28440baa96bcfced80af02c32c2e084bf4
Ak-shay <mittalakshay2000@gmail.com>
2019-03-30 10:48:46 +0530

    models added

--- waste/admin.py
+++ waste/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import People,Country,City,Bin,State,Worker
 
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(People)
+admin.site.register(Worker)
+admin.site.register(Bin)
 # Register your models here.
--- waste/models.py
+++ waste/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Country(models.Model):
+    country_name = models.CharField(max_length=30)
+
+class State(models.Model):
+    state_name = models.CharField(max_length=30)
+    country_name = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+class City(models.Model):
+    city_name = models.CharField(max_length=30)
+    state_name = models.ForeignKey(State, on_delete = models.CASCADE)
+
+class People(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    name = models.CharField(max_length = 50)
+    pic = models.ImageField()
+    city = models.ForeignKey(City,on_delete=models.CASCADE)
+
+class Worker(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    name = models.CharField(max_length = 50)
+    pic = models.ImageField()
+    city = models.ForeignKey(City,on_delete=models.CASCADE)
+
+class Bin(models.Model):
+    citizen = models.ForeignKey(People,on_delete=models.CASCADE)
+    green_waste = models.IntegerField()
+    blue_waste = models.IntegerField()
